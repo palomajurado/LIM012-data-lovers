41273dd708210b4f1d376c0c570a6b32996f97db
luzcry <tu-email@gmail.com>
2020-03-26 10:08:31 -0500

    arreglar problemas en data js

--- src/data.js
+++ src/data.js
@@ -1,3 +1,15 @@
+// evalua si el term es igual a 'ALL' si lo es retorna la data, sino retornar la data filtrada ok entiendo
+// return condition ? championList : filteredbyClass
+export const filteredbyClass = (championList, term) => {
+  let filteredbyClass = getArray(championList).filter(champion => {
+    if (champion.tags.indexOf(term) != -1) {
+      return champion;
+    }
+  });
+  return term === "ALL" ? championList : filteredbyClass;
+};
+
+
 export const filterByName = (championList, term) => {
   let error = TypeError;
   let filteredChampions = getArray(championList).filter(champion => {
@@ -8,7 +20,6 @@ export const filterByName = (championList, term) => {
   });
   //typeerror
   try {
-    if (term == " ") throw error;
     if (term == "0") throw error;
     if (term == null) throw error;
   } catch (error) {
@@ -17,45 +28,35 @@ export const filterByName = (championList, term) => {
   return filteredChampions;
 };
 
-// evalua si el term es igual a 'ALL' si lo es return la data, sino retornar la data filtrada
-// return condition ? championList : filtrdbyClass
-export const filteredbyClass = (championList, term) => {
-  const filteredbyClass1 = Object.values(championList).filter(champion => {
-    if (champion.tags.indexOf(term) !== -1) {
-      return champion;
-    }
-    return false;
-  });
-  return term === "ALL" ? championList : filteredbyClass1;
-};
+// evalua si el term es igual a 'ALL' si lo es retorna la data, sino retornar la data filtrada ok entiendo
+// return condition ? championList : filteredbyClass
+
 
 export const filteredByDifficulty = (championList, term) => {
-  let filteredByDifficult = Object.values(championList);
+  let filteredByDifficult;
+  const list = getArray(championList);
 
   if (term === "1") {
-    filteredByDifficult = Object.values(championList).filter(champion => {
+    filteredByDifficult = list.filter(champion => {
       if (champion.info.difficulty < 4) {
         return champion;
       }
-      return false;
     });
   }
 
   if (term === "2") {
-    filteredByDifficult = Object.values(championList).filter(champion => {
+    filteredByDifficult = list.filter(champion => {
       if (champion.info.difficulty > 3 && champion.info.difficulty < 7) {
         return champion;
       }
-      return false;
     });
   }
 
   if (term === "3") {
-    filteredByDifficult = Object.values(championList).filter(champion => {
+    filteredByDifficult = list.filter(champion => {
       if (champion.info.difficulty > 6) {
         return champion;
       }
-      return false;
     });
   }
 
@@ -91,4 +92,10 @@ export const orderList = (championList, term) => {
       .reverse();
   }
   return orderedList;
+};
+
+export const getArray = championList => {
+  return Array.isArray(championList) ?
+    championList :
+    Object.values(championList);
 };
