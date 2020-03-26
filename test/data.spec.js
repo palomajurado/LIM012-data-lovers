e40b4d168434af1e1df0f85d4a83eb445b0339f3
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2020-03-26 10:10:09 -0400

    Testeo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,55 +1,55 @@
 import {
-  filterByName,
-  filteredbyClass,
-  orderList,
-  filteredByDifficulty
+    filterByName,
+    filteredbyClass,
+    orderList,
+    filteredByDifficulty
 } from "../src/data.js";
 
 import data from "../src/data/lol/lol.js";
 let championList = data.data;
 describe("orderList", () => {
-  it("debería ser una función", () => {
-    expect(typeof orderList).toBe("function");
-  });
-  it("testeando sort", () => {
-    expect(orderList(championList, "az")).toStrictEqual(
-      Object.values(championList)
-    );
-  });
+    it("debería ser una función", () => {
+        expect(typeof orderList).toBe("function");
+    });
+    it("testeando sort", () => {
+        expect(orderList(championList, "az")).toStrictEqual(
+            Object.values(championList)
+        );
+    });
 });
 //funcion filtrado por nombre
 describe("filterByName", () => {
-  it("debería ser una función", () => {
-    expect(typeof filterByName).toBe("function");
-  });
-  it("debería retornar Campeon Morgana", () => {
-    expect(filterByName(championList, "morgana")).toStrictEqual([
-      championList.Morgana
-    ]);
-  });
-
-  //funcion filtrado por clase
-  describe("filteredbyClass", () => {
     it("debería ser una función", () => {
-      expect(typeof filteredbyClass).toBe("function");
+        expect(typeof filterByName).toBe("function");
     });
-  });
+    it("debería retornar Campeon Morgana", () => {
+        expect(filterByName(championList, "morgana")).toStrictEqual([
+            championList.Morgana
+        ]);
+    });
+
+    //funcion filtrado por clase
+    describe("filteredbyClass", () => {
+        it("debería ser una función", () => {
+            expect(typeof filteredbyClass).toBe("function");
+        });
+    });
 });
 
 //funcion filtrar por dificultad
 it("debería ser una función dificultad", () => {
-  expect(typeof filteredByDifficulty).toBe("function");
+    expect(typeof filteredByDifficulty).toBe("function");
 });
 
 it("debería retornar dificultad data-value 1", () => {
-  let filteredChampions = filteredByDifficulty(championList, "1");
-  expect(filteredChampions.length).toBe(20);
+    let filteredChampions = filteredByDifficulty(championList, "1");
+    expect(filteredChampions.length).toBe(20);
 });
 it("debería retornar  dificultad data-value 2", () => {
-  let filteredChampions = filteredByDifficulty(championList, "2");
-  expect(filteredChampions.length).toBe(64);
+    let filteredChampions = filteredByDifficulty(championList, "2");
+    expect(filteredChampions.length).toBe(64);
 });
 it("debería retornar dificultad data-value 3", () => {
-  let filteredChampions = filteredByDifficulty(championList, "3");
-  expect(filteredChampions.length).toBe(50);
+    let filteredChampions = filteredByDifficulty(championList, "3");
+    expect(filteredChampions.length).toBe(50);
 });
