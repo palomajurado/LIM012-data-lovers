59120cb4aaa86f0349b814406ee383ef84aacd38
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2020-03-27 10:57:18 -0400

    filterbydificulty

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line arrow-body-style
 export const getArray = (championList) => {
-  return Array.isArray(championList) ? championList : Object.values(championList);
+    return Array.isArray(championList) ? championList : Object.values(championList);
 };
 
 // evalua si el term es igual a 'ALL' si lo es retorna la data,
@@ -8,26 +8,26 @@ export const getArray = (championList) => {
 // return condition ? championList : filteredbyClass
 
 export const filteredbyClass = (championList, term) => {
-  // eslint-disable-next-line
-  const filteredbyClassArray = getArray(championList).filter((champion) => {
-    if (champion.tags.indexOf(term) !== -1) {
-      return champion;
-    }
-  });
-  return term === 'ALL' ? championList : filteredbyClassArray;
+    // eslint-disable-next-line
+    const filteredbyClassArray = getArray(championList).filter((champion) => {
+        if (champion.tags.indexOf(term) !== -1) {
+            return champion;
+        }
+    });
+    return term === 'ALL' ? championList : filteredbyClassArray;
 };
 
 
 export const filterByName = (championList, term) => {
-  // eslint-disable-next-line
-  let filteredChampions = getArray(championList).filter(champion => {
-    // indexof da -1 si no encuentra resultados
-    if (champion.name.toLowerCase().indexOf(term) !== -1) {
-      return champion;
-    }
-  });
+    // eslint-disable-next-line
+    let filteredChampions = getArray(championList).filter(champion => {
+        // indexof da -1 si no encuentra resultados
+        if (champion.name.toLowerCase().indexOf(term) !== -1) {
+            return champion;
+        }
+    });
 
-  return filteredChampions;
+    return filteredChampions;
 };
 
 // evalua si el term es igual a 'ALL' si lo es retorna la data,
@@ -36,66 +36,67 @@ export const filterByName = (championList, term) => {
 
 
 export const filteredByDifficulty = (championList, term) => {
-  let filteredByDifficult;
-  const list = getArray(championList);
+    const list = getArray(championList);
+    let filteredByDifficult = list;
 
-  if (term === '1') {
-    // eslint-disable-next-line
-    filteredByDifficult = list.filter(champion => {
-      if (champion.info.difficulty < 4) {
-        return champion;
-      }
-    });
-  }
+    if (term === '1') {
+        // eslint-disable-next-line
+        filteredByDifficult = list.filter(champion => {
+            if (champion.info.difficulty < 4) {
+                return champion;
+            }
+        });
+    }
 
-  if (term === '2') {
-    // eslint-disable-next-line
-    filteredByDifficult = list.filter(champion => {
-      if (champion.info.difficulty > 3 && champion.info.difficulty < 7) {
-        return champion;
-      }
-    });
-  }
+    if (term === '2') {
+        // eslint-disable-next-line
+        filteredByDifficult = list.filter(champion => {
+            if (champion.info.difficulty > 3 && champion.info.difficulty < 7) {
+                return champion;
+            }
+        });
+    }
 
-  if (term === '3') {
-    // eslint-disable-next-line
-    filteredByDifficult = list.filter(champion => {
-      if (champion.info.difficulty > 6) {
-        return champion;
-      }
-    });
-  }
+    if (term === '3') {
+        // eslint-disable-next-line
+        filteredByDifficult = list.filter(champion => {
+            if (champion.info.difficulty > 6) {
+                return champion;
+            }
+        });
+    }
 
-  return filteredByDifficult;
+    return filteredByDifficult;
 };
 
 export const orderList = (championList, term) => {
-  let orderedList;
-  const list = getArray(championList);
-
-  if (term === 'az') {
-    orderedList = list.sort((a, b) => {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
-  }
-  if (term === 'za') {
-    orderedList = list
-      .sort((a, b) => {
-        if (a.name < b.name) {
-          return -1;
-        }
-        if (a.name > b.name) {
-          return 1;
-        }
-        return 0;
-      })
-      .reverse();
-  }
-  return orderedList;
+    let orderedList;
+    const list = getArray(championList);
+    list.innerHTML = '';
+    if (term === 'az') {
+        orderedList = list.sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    list.innerHTML = '';
+    if (term === 'za') {
+        orderedList = list
+            .sort((a, b) => {
+                if (a.name < b.name) {
+                    return -1;
+                }
+                if (a.name > b.name) {
+                    return 1;
+                }
+                return 0;
+            })
+            .reverse();
+    }
+    return orderedList;
 };
