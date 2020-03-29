029a448a9c0ae2e9a19de0a7d475d6e1be466a8f
Paloma Jurado <51180179+sambasilio@users.noreply.github.com>
2020-03-29 02:24:43 -0400

    Resolucion de BUGS *Boton up *Eslint *Testing not working *Difficulty function

--- src/data.js
+++ src/data.js
@@ -1,102 +1,93 @@
-// eslint-disable-next-line arrow-body-style
 export const getArray = (championList) => {
-    return Array.isArray(championList) ? championList : Object.values(championList);
+  if (!Array.isArray(championList)) {
+    return Object.values(championList);
+  }
+  return championList;
 };
-
-// evalua si el term es igual a 'ALL' si lo es retorna la data,
-// sino retornar la data filtrada ok entiendo
-// return condition ? championList : filteredbyClass
 
 export const filteredbyClass = (championList, term) => {
-    // eslint-disable-next-line
-    const filteredbyClassArray = getArray(championList).filter((champion) => {
-        if (champion.tags.indexOf(term) !== -1) {
-            return champion;
-        }
-    });
-    return term === 'ALL' ? championList : filteredbyClassArray;
+  const filteredbyClassArray = getArray(championList).filter((champion) => {
+    if (champion.tags.indexOf(term) !== -1) {
+      return champion;
+    }
+    return false;
+  });
+  return term === 'ALL' ? championList : filteredbyClassArray;
 };
-
 
 export const filterByName = (championList, term) => {
-    // eslint-disable-next-line
-    let filteredChampions = getArray(championList).filter(champion => {
-        // indexof da -1 si no encuentra resultados
-        if (champion.name.toLowerCase().indexOf(term) !== -1) {
-            return champion;
-        }
-    });
+  const filteredChampions = getArray(championList).filter((champion) => {
+    if (champion.name.toLowerCase().indexOf(term) !== -1) {
+      return champion;
+    }
+    return false;
+  });
 
-    return filteredChampions;
+  return filteredChampions;
 };
 
-// evalua si el term es igual a 'ALL' si lo es retorna la data,
-// sino retornar la data filtrada ok entiendo
-// return condition ? championList : filteredbyClass
-
-
 export const filteredByDifficulty = (championList, term) => {
-    const list = getArray(championList);
-    let filteredByDifficult = list;
+  const list = getArray(championList);
+  let filteredByDifficult = list;
 
-    if (term === '1') {
-        // eslint-disable-next-line
-        filteredByDifficult = list.filter(champion => {
-            if (champion.info.difficulty < 4) {
-                return champion;
-            }
-        });
-    }
+  if (term === '1') {
+    filteredByDifficult = list.filter((champion) => {
+      if (champion.info.difficulty < 4) {
+        return champion;
+      }
+      return false;
+    });
+  }
 
-    if (term === '2') {
-        // eslint-disable-next-line
-        filteredByDifficult = list.filter(champion => {
-            if (champion.info.difficulty > 3 && champion.info.difficulty < 7) {
-                return champion;
-            }
-        });
-    }
+  if (term === '2') {
+    filteredByDifficult = list.filter((champion) => {
+      if (champion.info.difficulty > 3 && champion.info.difficulty < 7) {
+        return champion;
+      }
+      return false;
+    });
+  }
 
-    if (term === '3') {
-        // eslint-disable-next-line
-        filteredByDifficult = list.filter(champion => {
-            if (champion.info.difficulty > 6) {
-                return champion;
-            }
-        });
-    }
+  if (term === '3') {
+    filteredByDifficult = list.filter((champion) => {
+      if (champion.info.difficulty > 6) {
+        return champion;
+      }
+      return false;
+    });
+  }
 
-    return filteredByDifficult;
+  return filteredByDifficult;
 };
 
 export const orderList = (championList, term) => {
-    let orderedList;
-    const list = getArray(championList);
-    list.innerHTML = '';
-    if (term === 'az') {
-        orderedList = list.sort((a, b) => {
-            if (a.name < b.name) {
-                return -1;
-            }
-            if (a.name > b.name) {
-                return 1;
-            }
-            return 0;
-        });
-    }
-    list.innerHTML = '';
-    if (term === 'za') {
-        orderedList = list
-            .sort((a, b) => {
-                if (a.name < b.name) {
-                    return -1;
-                }
-                if (a.name > b.name) {
-                    return 1;
-                }
-                return 0;
-            })
-            .reverse();
-    }
-    return orderedList;
+  let orderedList;
+  const list = getArray(championList);
+  list.innerHTML = '';
+  if (term === 'az') {
+    orderedList = list.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+  list.innerHTML = '';
+  if (term === 'za') {
+    orderedList = list
+      .sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      })
+      .reverse();
+  }
+  return orderedList;
 };
